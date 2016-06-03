2437b0820db861a37552d0b8101c61442e143e88
Soumya Ray <soumya.ray@gmail.com>
2016-06-03 18:25:37 +0800

    Renamed variables and made lines shorter

--- ed25519_sign_verify.rb
+++ ed25519_sign_verify.rb
@@ -4,14 +4,16 @@ require 'jose'
 new_app_secret = JOSE::JWK.generate_key([:okp, :Ed25519])
 new_app_public = new_app_secret.to_public
 
-saved_secret_key = Base64.strict_encode64(new_app_secret.to_okp[1]) # saved by App
-saved_public_key = Base64.strict_encode64(new_app_public.to_okp[1]) # sent to API
+app_secret_key = Base64.strict_encode64(new_app_secret.to_okp[1]) # saved by App
+app_public_key = Base64.strict_encode64(new_app_public.to_okp[1]) # saved by API
 
 # 1. App uses secret key to sign message for API
-app_secret_key = JOSE::JWK.from_okp([:Ed25519, Base64.decode64(saved_secret_key)])
+app_secret_key = JOSE::JWK.from_okp(
+  [:Ed25519, Base64.strict_decode64(app_secret_key)])
 message = 'messages is here'
 msg_digest = app_secret_key.sign(message).compact
 
 # 2. API receives message and verifies sender using App's public key
-app_public_key = JOSE::JWK.from_okp([:Ed25519, Base64.decode64(saved_public_key)])
+app_public_key = JOSE::JWK.from_okp(
+  [:Ed25519, Base64.strict_decode64(app_public_key)])
 verified, jwt, jws = app_public_key.verify(msg_digest)
